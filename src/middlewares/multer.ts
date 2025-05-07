fa629744574ee1a44e227ba802bdf77d54798ce3
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2025-05-07 18:32:39 -0300

    use diskStorage on multer

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,3 +1,14 @@
 import multer from 'multer'
 
-export const productImageUpload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        const fileName = Date.now()
+        cb(null, fileName + '.png')
+    }
+})
+
+export const productImageUpload = multer({ storage: storage })
+
